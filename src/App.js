0c8931fc18938a3464419178584fdb26b4b1960f
Imanbeq <imanqlove@gmail.com>
2025-04-20 15:45:25 +0600

    added components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React from "react";
+import "./scss/app.scss";
+import Header from "./components/Header";
+import Categories from "./components/Categories";
+import Sort from "./components/Sort";
+import PizzaBlock from "./components/PizzaBlock";
 
 function App() {
   return (
-    <>
-      <h1>Hello world</h1>
-    </>
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+            <PizzaBlock title='Кола-барбекью' price='500'/>
+            <PizzaBlock title='Пеперони' price='700'/>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
